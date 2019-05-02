39746c9b1e520b79d32e9017b134fbc13f9fa37e
Choi, Wanbok <wanbok.choi@linecorp.com>
2019-05-02 20:41:55 +0900

    Change theme color which looks comfortable

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class AutoClipApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: Main(title: 'Flutter Demo Home Page'),
     );
